f84f602bde1e8d86910d281bfc37f2a292d544e1
anyya <152702831+anikiwii@users.noreply.github.com>
2023-12-03 22:17:00 +0100

    Update work-website.py
    changed some things

--- work-website.py
+++ work-website.py
@@ -8,17 +8,29 @@ def dodaj_zlecenie(nazwa, lokalizacja, cena):
     }
     zlecenia.append(zlecenie)
 
-def wyswietl_zlecenia():
-    print("Dostępne zlecenia:")
-    for idx, zlecenie in enumerate(zlecenia, start=1):
-        print(f"{idx}. {zlecenie['nazwa']} - {zlecenie['lokalizacja']} - {zlecenie['cena']}")
+def wyswietl_nowe_zlecenia():
+    if len(zlecenia) > 0:
+        print("Nowe zlecenia:")
+        for idx, zlecenie in enumerate(zlecenia[-1:], start=len(zlecenia)):
+            print(f"{idx}. {zlecenie['nazwa']} - {zlecenie['lokalizacja']} - {zlecenie['cena']}")
+    else:
+        print("Brak nowych zleceń.")
+
+def wyswietl_wszystkie_zlecenia():
+    if len(zlecenia) > 0:
+        print("Wszystkie zlecenia:")
+        for idx, zlecenie in enumerate(zlecenia, start=1):
+            print(f"{idx}. {zlecenie['nazwa']} - {zlecenie['lokalizacja']} - {zlecenie['cena']}")
+    else:
+        print("Brak dostępnych zleceń.")
 
 while True:
     print("\n1. Dodaj zlecenie")
-    print("2. Wyświetl zlecenia")
-    print("3. Wyjdź")
+    print("2. Wyświetl nowe zlecenia")
+    print("3. Wyświetl wszystkie zlecenia")
+    print("4. Wyjdź")
 
-    wybor = input("Wybierz opcję (1/2/3): ")
+    wybor = input("Wybierz opcję (1/2/3/4): ")
 
     if wybor == '1':
         nazwa = input("Podaj nazwę zlecenia: ")
@@ -29,9 +41,12 @@ while True:
         print("Zlecenie dodane pomyślnie.")
 
     elif wybor == '2':
-        wyswietl_zlecenia()
+        wyswietl_nowe_zlecenia()
 
     elif wybor == '3':
+        wyswietl_wszystkie_zlecenia()
+
+    elif wybor == '4':
         break
 
     else:
